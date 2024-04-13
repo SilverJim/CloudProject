906ef6a8707209297e5fe18dfbb3fbc3965225b9
Jian Guan <guanwwjiam@gmail.com>
2024-04-13 13:05:17 -0400

    add files

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 hadoop fs -put /opt/data /raw_data
 spark-submit --master yarn --deploy-mode cluster --driver-memory 1g --executor-memory 1g --executor-cores 1 --conf "spark.pyspark.python=/opt/bitnami/python/bin/python3" /opt/data_processing.py
 hadoop fs -ls /processed_data
--- start.ps1
+++ start.ps1
@@ -1,3 +1,4 @@
+docker-compose kill
 docker-compose up -d
 docker exec -it cloudproject-spark-1 /opt/start-hadoop.sh
 docker exec -it cloudproject-spark-client-1 /opt/run.sh
