93e1b8f3f415ced17a75c98dec54c2a693847736
Jian Guan <guanwwjiam@gmail.com>
2024-04-13 12:51:24 -0400

    add docker-compose.yml

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 hdfs dfs -put /opt/data /raw_data
-spark-submit --master yarn --deploy-mode cluster --driver-memory 1g --executor-memory 1g --executor-cores 1 --conf "spark.pyspark.python=/path/to/python3" /opt/data_processing.py
+spark-submit --master yarn --deploy-mode cluster --driver-memory 1g --executor-memory 1g --executor-cores 1 --conf "spark.pyspark.python=/opt/bitnami/python/bin/python3" /opt/data_processing.py
