5278f5532a0f1e2c7e7847109909cf37b9b2dc61
Jian Guan <guanwwjiam@gmail.com>
2024-04-13 13:30:17 -0400

    add files

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+hadoop fs -rm -r /raw_data /processed_data
 hadoop fs -put /opt/data/* /raw_data
 spark-submit --master yarn --deploy-mode cluster --driver-memory 1g --executor-memory 1g --executor-cores 1 --conf "spark.pyspark.python=/opt/bitnami/python/bin/python3" /opt/data_processing.py
 hadoop fs -ls /processed_data
