9e0350fac21330d995e04412f09558c02f12ca01
Jian Guan <guanwwjiam@gmail.com>
2024-04-14 02:40:11 -0400

    debug

--- start.ps1
+++ start.ps1
@@ -2,6 +2,3 @@ docker-compose kill
 docker-compose rm -f
 docker-compose build
 docker-compose up -d
-
-# echo "/opt/start-hadoop.sh"| docker exec -i cloudproject-spark-1 bash
-# docker exec -it cloudproject-spark-client-1 /opt/run.sh
