4d7db0b82b99543a893da3fd1b15f93d159c0aa3
Jian Guan <guanwwjiam@gmail.com>
2024-04-13 22:30:54 -0400

    add files

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,3 @@ USER root
 COPY data /opt/data
 COPY run.sh /opt/run.sh
 COPY data_processing.py /opt/data_processing.py
-CMD ["sleep", "infinity"]
--- start.ps1
+++ start.ps1
@@ -1,5 +1,5 @@
 docker-compose kill
 docker-compose build
 docker-compose up -d
-docker exec -it cloudproject-spark-1 /opt/start-hadoop.sh
+# docker exec -it cloudproject-spark-1 bash -c "source /etc/profile; source ./.bashrc; ./start-hadoop.sh"
 docker exec -it cloudproject-spark-client-1 /opt/run.sh
